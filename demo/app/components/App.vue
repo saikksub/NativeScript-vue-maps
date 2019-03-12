bb8072fcdd89c2f633cda1a14d89586ba43f8130
saikksub <saikksub@gmail.com>
2019-03-12 19:15:07 +0530

    update

--- demo/app/components/App.vue
+++ demo/app/components/App.vue
@@ -2,35 +2,32 @@
     <Page>
         <ActionBar title="Welcome to NativeScript-Vue!"/>
         <StackLayout>
-          <Mapbox
-              accessToken="pk.eyJ1Ijoic2Fpa2tzdWIiLCJhIjoiY2pzd3gwZ3R6MGZ4MTQzczFkeDByamF0NiJ9.0rU2RC4Re00MvQ3ObLVVaA"
-              mapStyle="traffic_day"
-              latitude="28.614809"
-              longitude="77.209086"
-              hideCompass="true"
-              zoomLevel="8"
-              showUserLocation="false"
-              disableZoom="false"
-              disableRotation="false"
-              disableScroll="false"
-              disableTilt="false"
-              @mapReady="onMapReady($event)">
-          </Mapbox>
+          <maps
+            :onMapReady="onMapReady" />
         </StackLayout>
     </Page>
 </template>
 
 <script>
+  import Maps from '@/components/Maps'
+  const Color = require('tns-core-modules/color')
+
   export default {
+    components: {
+      Maps
+    },
     data() {
       return {
         msg: 'Hello World!'
       }
     },
+    mounted () {
+      this.$root.$emit('add')
+    },
     methods: {
       onMapReady(args) {
-          console.log(args.map.addMarkers)
-          args.map.addMarkers([
+          // Add Marker
+          /* this.$root.$emit('addMarker', ([
               {
                   lat: 28.614809,
                   lng: 77.209086,
@@ -49,7 +46,74 @@
                   title: "Rajpath Area",
                   subtitle: "New Delhi is an urban district of Delhi which serves as the capital of India."
               }
-          ]);
+          ])) */
+          // Fix current location
+          /* this.$root.$emit('fixCurrentLocation', {
+            desiredAccuracy: 3,
+            updateDistance: 10,
+            maximumAge: 20000,
+            timeout: 20000
+          }) */
+          // Add Polygon
+          /* this.$root.$emit('addPolygon', {
+            id: 1, // optional, can be used in 'removePolygons'
+            fillOpacity: 0.7,
+
+            // stroke-related properties are only effective on iOS
+            strokeWidth: 8,
+            strokeOpacity: 0.5,
+
+            points: [
+              {
+                lat: 52.3923633970718,
+                lng: 4.902648925781249
+              },
+              {
+                lat: 52.35421556258807,
+                lng: 4.9308013916015625
+              },
+              {
+                lat: 52.353796172573944,
+                lng: 4.8799896240234375
+              },
+              {
+                lat: 52.3864966440161,
+                lng: 4.8621368408203125
+              },
+              {
+                lat: 52.3923633970718,
+                lng: 4.902648925781249
+              }
+            ]
+          }) */
+          this.$root.$emit('addPolyline', {
+            id: 1, // optional, can be used in 'removePolylines'
+            color: '#000000', // Set the color of the line (default black)
+            width: 5, // Set the width of the line (default 5)
+            opacity: 1, //Transparency / alpha, ranging 0-1. Default fully opaque (1).
+            points: [
+                {
+                    'lat': 52.3833160, // mandatory
+                    'lng': 4.8991780 // mandatory
+                },
+                {
+                    'lat': 52.3834160,
+                    'lng': 4.8991880
+                },
+                {
+                    'lat': 52.3835160,
+                    'lng': 5.8991980
+                },
+                {
+                    'lat': 52.3835160,
+                    'lng': 6.8991980
+                },
+                {
+                    'lat': 52.3835160,
+                    'lng': 7.8991980
+                }
+            ]
+          })
       }
     }
   }
---
+++ demo/app/components/Maps.vue
@@ -0,0 +1,104 @@
+<template>
+  <Mapbox
+      ref="appMapBox"
+      :accessToken="token"
+      :mapStyle="mapStyle"
+      latitude="0"
+      longitude="0"
+      :hideCompass="hideCompass"
+      :zoomLevel="zoomLevel"
+      :showUserLocation="showUserLocation"
+      :disableZoom="disableZoom"
+      :disableRotation="disableRotation"
+      :disableScroll="disableScroll"
+      :disableTilt="disableTilt"
+      @mapReady="onMapReady($event)">
+  </Mapbox>
+</template>
+
+<script>
+const geolocation = require("nativescript-geolocation")
+
+export default {
+  props: {
+    onMapReady: {
+      type: Function,
+      required: true,
+      default: () => {}
+    }
+  },
+  data () {
+    return {
+      title: '',
+      subtitle: '',
+      mapStyle: 'traffic_day',
+      token: 'pk.eyJ1Ijoic2Fpa2tzdWIiLCJhIjoiY2pzd3gwZ3R6MGZ4MTQzczFkeDByamF0NiJ9.0rU2RC4Re00MvQ3ObLVVaA',
+      hideCompass: false,
+      zoomLevel: '8',
+      showUserLocation: true,
+      disableZoom: false,
+      disableRotation: false,
+      disableScroll: false,
+      disableTilt: false
+    }
+  },
+  mounted () {
+    const self = this
+    this.$root.$on('fixCurrentLocation', (options) => {
+      // Options are must to get current location of the user. Please visit: https://docs.nativescript.org/hardware/location#getcurrentlocation
+      if (options && options.constructor === {}.constructor) {
+        geolocation.getCurrentLocation(options).then((location) => {
+          self.$root.$emit('viewLocation', {
+            lat: location.latitude,
+            lng: location.longitude
+          })
+        }).catch((error) => {
+          console.log(error)
+        })
+      }
+    })
+    this.$root.$on('viewLocation', ({ lat, lng, zoomLevel }) => {
+      self.$refs.appMapBox.nativeView.animateCamera({
+        target: {
+          lat,
+          lng,
+        },
+        zoomLevel: zoomLevel || 20,
+        altitude: 2000, // iOS (meters from the ground)
+        bearing: 270, // Where the camera is pointing, 0-360 (degrees)
+        tilt: 50,
+        duration: 5000
+      })
+    })
+    this.$root.$on('addMarker', function (data) {
+      if (data && data.constructor === [].constructor) {
+        self.$refs.appMapBox.nativeView.addMarkers(data)
+      }
+    })
+    this.$root.$on('startFollowingUser', function ({ mode, animated }) {
+      self.$refs.appMapBox.nativeView.trackUser({
+        mode: mode || 'FOLLOW_WITH_HEADING',
+        animated: animated && typeof animated === 'boolean' ? animated : true
+      })
+    }),
+    this.$root.$on('removeMarkers', function () {
+      self.$refs.appMapBox.nativeView.removeMarkers()
+    }),
+    this.$root.$on('addPolygon', function (data) {
+      self.$refs.appMapBox.nativeView.addPolygon(data)
+      .then(() => {
+        self.$root.$emit('viewLocation', data.points[0])
+      })
+      .catch((error) => console.log("mapbox addPolygon error: " + error));
+    })
+    this.$root.$on('addPolyline', function (data) {
+      self.$refs.appMapBox.nativeView.addPolyline(data)
+      .then(() => {
+        self.$root.$emit('viewLocation', Object.assign({}, { zoomLevel: 10 }, data.points[0]))
+      })
+      .catch((error) => console.log("mapbox addPolygon error: " + error));
+    })
+  }
+}
+</script>
+
